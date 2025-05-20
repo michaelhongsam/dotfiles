18694dba119c698ad4a904f6de7078ae3497ecbb
Michael Sam <msam@forthepeople.com>
2025-05-20 15:29:58 +0000

    added vscode extensions

---
+++ run_once.sh
@@ -0,0 +1,6 @@
+code --install-extension eamodio.gitlens --force
+code --install-extension eeyore.yapf --force
+code --install-extension hashicorp.terraform --force
+code --install-extension mtxr.sqltools --force
+code --install-extension mechatroner.rainbow-csv --force
+
